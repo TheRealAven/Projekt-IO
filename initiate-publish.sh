8d0d7fa5e1e04c43e57de80b111de631327d3e67
travis-ci <travis@travis-ci.org>
2018-01-20 23:03:29 +0100

    v13 nowy token

--- initiate-publish.sh
+++ initiate-publish.sh
@@ -7,7 +7,7 @@ cp -R doc/ $HOME/javadoc-latest
 cd $HOME
 git config --global user.email "travis@travis-ci.org"
 git config --global user.name "travis-ci"
-git clone --quiet --branch=gh-pages https://022889ebfd39c06854645c7dfc914e67661385cd@github.com/TheRealAven/Projekt-IO gh-pages > /dev/null
+git clone --quiet --branch=gh-pages https://${GH_TOKEN}@github.com/TheRealAven/Projekt-IO gh-pages > /dev/null
 
 echo -e "Commiting... \n"
 # Commit and Push the Changes
@@ -16,7 +16,7 @@ git rm -rf ./javadoc
 cp -Rf $HOME/javadoc-latest ./javadoc
 git add -f .
 git commit -m "Lastest javadoc on successful travis build $TRAVIS_BUILD_NUMBER auto-pushed to gh-pages"
-git push -fq origin gh-pages
+git push -fq origin gh-pages > /dev/null
 
 echo -e "Published Javadoc to gh-pages.\n"
 fi
