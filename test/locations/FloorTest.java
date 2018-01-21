d684afc5ed2151f3d73a4b6eaa1e1842b5805c5e
Szczukox <33254494+Szczukox@users.noreply.github.com>
2018-01-21 18:42:17 +0100

    Add FloorTest.java

---
+++ test/locations/FloorTest.java
@@ -0,0 +1,60 @@
+package locations;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class FloorTest {
+
+    @Test
+    public void countLightningPower() {
+        Floor testFloor = new Floor(23);
+        testFloor.addRoom(new Room(1, 10, 10, 10, 10));
+        assertEquals(1.0, testFloor.countLightningPower(), 0.0);
+
+        testFloor.addRoom(new Room(2, 10, 10, 10, 10));
+        assertEquals(1.0, testFloor.countLightningPower(), 0.0);
+
+        testFloor.addRoom(new Room(3, 10, 10, 10, 10));
+        assertEquals(1.0, testFloor.countLightningPower(), 0.0);
+    }
+
+    @Test
+    public void countTotalArea() {
+        Floor testFloor = new Floor(23);
+        testFloor.addRoom(new Room(1, 10, 10, 10, 10));
+        assertEquals(10.0, testFloor.countTotalArea(), 0.0);
+
+        testFloor.addRoom(new Room(2, 10, 10, 10, 10));
+        assertEquals(20.0, testFloor.countTotalArea(), 0.0);
+
+        testFloor.addRoom(new Room(3, 10, 10, 10, 10));
+        assertEquals(30.0, testFloor.countTotalArea(), 0.0);
+    }
+
+    @Test
+    public void countTotalCubage() {
+        Floor testFloor = new Floor(23);
+        testFloor.addRoom(new Room(1, 10, 10, 10, 10));
+        assertEquals(10.0, testFloor.countTotalCubage(), 0.0);
+
+        testFloor.addRoom(new Room(2, 10, 10, 10, 10));
+        assertEquals(20.0, testFloor.countTotalCubage(), 0.0);
+
+        testFloor.addRoom(new Room(3, 10, 10, 10, 10));
+        assertEquals(30.0, testFloor.countTotalCubage(), 0.0);
+    }
+
+    @Test
+    public void countEnergyConsumption() {
+        Floor testFloor = new Floor(23);
+        testFloor.addRoom(new Room(1, 10, 10, 10, 10));
+        assertEquals(1.0, testFloor.countEnergyConsumption(), 0.0);
+        
+        testFloor.addRoom(new Room(2, 10, 10, 10, 10));
+        assertEquals(1.0, testFloor.countEnergyConsumption(), 0.0);
+
+        testFloor.addRoom(new Room(3, 10, 10, 10, 10));
+        assertEquals(1.0, testFloor.countEnergyConsumption(), 0.0);
+    }
+}
